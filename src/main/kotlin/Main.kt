6abed06bb755428a19903ccde8d5fbaba51e7c6c
fbatlos <bdlsfrancisco@iesrafaelalberti.es>
2024-01-23 18:47:07 +0100

    jas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,5 @@
+import kotlin.collections.mutableListOf as mutableListOf
+
 /*
 Desarrolla un programa con las siguientes clases:
 
@@ -41,18 +43,68 @@ La sobrecarga de métodos facilita la legibilidad del código y proporciona flex
 método para realizar operaciones similares pero con diferentes conjuntos de datos.
 
 */
+class cafetera(var ubicacion: String){
+    var capacidad = 1000
+    var cantidad = 0
+    constructor(ubicacion: String , capacidad:Int) : this(ubicacion) {
+        if (cantidad>capacidad){
+            cantidad = capacidad
+        }
+    }
+    fun llenar(){
+        this.cantidad = this.capacidad
+    }
+
+    fun servirTaza(taza: MutableList<Int>) {
+        val eleccion = 1..10
+        if (cantidad>=100){
+            this.cantidad -= taza[eleccion.random()]//seleccionamos una taza aleatoria
+        }else{
+            var falta = taza[eleccion.random()]
+            falta -= this.cantidad
+        }
+    }
+
+    fun vaciar(){
+        this.cantidad = 0
+    }
+
+    fun agregarCafe(añade:Int){
+        if (añade>200){
+            this.cantidad = this.capacidad
+        }else{this.cantidad += añade}
+    }
+
+    override fun toString(): String {
+        return "Cafetera(ubicación = $ubicacion, capacidad = $capacidad c.c., cantidad = $cantidad c.c.)"
+    }
+
+}
+
+
 
 fun main() {
 
     //TODO: Crear 3 cafeteras en la Sala, Cocina y Oficina
-
+    val cafetera1 = cafetera("Sala" , 1000)
+    val cafetera2 = cafetera("Cocina" , 1000)
+    val cafetera3 = cafetera("Oficina" , 1000)
 
     //TODO: Crear una lista de 20 tazas con capacidades aleatorias
-
+    var cont = 1
+    
+    do {
+       
+        val taza = mutableListOf()
+        taza += crarTaza()
+        cont++
+    }while (cont!=20)
    
     println("**********************************************")
     //TODO: Mostrar por pantalla el contenido de las 3 cafeteras y las tazas.
-
+    println(cafetera1.toString())
+    println(cafetera2.toString())
+    println(cafetera3.toString())
     
     println("**********************************************")
     println("Llenar la cafetera1 de café...")
@@ -61,29 +113,38 @@ fun main() {
     println("Agregar 400 c.c. de café a la cafereta3...")
 
     //TODO: Llenar la cafetera1 de café.
-
+        cafetera1.llenar()
 
     //TODO: Vaciar la cafetera2.
-
+        cafetera2.vaciar()
 
     //TODO: Agregar café a la cafetera2 a la mitad de su capacidad.
-
+        cafetera2.agregarCafe(200)
+        cafetera2.agregarCafe(200)
+        cafetera2.agregarCafe(100)
 
     //TODO: Agregar 400 c.c. de café a la cafereta3
-
+        cafetera3.agregarCafe(400)
 
     println("**********************************************")
     //TODO: Mostrar por pantalla el contenido de las 3 cafeteras
-
+        println(cafetera3.toString())
 
     println("**********************************************")
     println("Servir café en las tazas...")
 
     //TODO: Servir café en las tazas... siempre que haya café en la cafetera y en el orden cafetera1, cafetera2 y cafetera3.
-
-
+            cafetera1.servirTaza(taza)
+            cafetera2.servirTaza(taza)
+            cafetera3.servirTaza(taza)
     println("**********************************************")
     //TODO: Mostrar por pantalla el contenido de las 3 cafeteras y las tazas.
 
 
+}
+
+fun crarTaza():Int{
+    val contenido = 100..150
+    return  contenido.random()
+    
 }
